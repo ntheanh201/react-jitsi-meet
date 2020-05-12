409af2f45e041b9a98f6ae92cf04cd28a61a08d9
ntheanh201 <ntheanh201@gmail.com>
2020-05-12 20:17:12 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [value, setValue] = useState(null)
+  const onSubmit = () => {
+    console.log('foo')
+    return axios
+      .post(`https://alpha.jitsi.net/http-pre-bind?room=${value}`)
+      .then(function (response) {
+        console.log(response)
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <input type='text' onChange={(event) => setValue(event.target.value)} />
+      <button onClick={onSubmit}>Generate</button>
+    </>
+  )
 }
 
-export default App;
+export default App
