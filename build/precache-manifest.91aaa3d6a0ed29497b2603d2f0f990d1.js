f3a2b82e7a448f5c69ef79778a75140147368bf2
The Anh <ntheanh201@gmail.com>
2020-06-02 12:53:35 +0700

    build

---
+++ build/precache-manifest.91aaa3d6a0ed29497b2603d2f0f990d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16679487d3eea7c1c3fca23872feded6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "66dae6f1e98dde3a0118",
+    "url": "./static/css/main.3bf2d5a5.chunk.css"
+  },
+  {
+    "revision": "3ed9759e4c8d20a6a1f2",
+    "url": "./static/js/2.c4e82903.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.c4e82903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66dae6f1e98dde3a0118",
+    "url": "./static/js/main.c9a1215b.chunk.js"
+  },
+  {
+    "revision": "aaae8955472b60f39908",
+    "url": "./static/js/runtime-main.3dbf761f.js"
+  }
+]);
